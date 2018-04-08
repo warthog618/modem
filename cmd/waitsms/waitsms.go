5e1ad2606ee501b2df6e75307dc92a63de135e8b
Kent Gibson <warthog618@gmail.com>
2018-04-08 16:34:43 +0800

    Add decoding of received SMS messages, including UCS-2 emoticons 😁. Add reassembly of concatenated SMSs. Add signal quality polling.

--- cmd/waitsms/waitsms.go
+++ cmd/waitsms/waitsms.go
@@ -1,9 +1,10 @@
 // waitsms waits for SMSs to be received by the modem, and dumps them to stdout.
 //
-// The output is currently in undecoded PDU format.
-//
 // This provides an example of using indications, as well as a test
 // that the library works with the modem.
+//
+// The modem device provided must support nofications, or no SMSs will be seen.
+// (the notification port is typically USB2, hence the default)
 package main
 
 import (
@@ -12,7 +13,16 @@ import (
 	"io"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 	"time"
+
+	"github.com/warthog618/sms/ms/message"
+	"github.com/warthog618/sms/ms/sar"
+
+	"github.com/warthog618/sms/encoding/tpdu"
+
+	"github.com/warthog618/sms/ms/pdumode"
 
 	"github.com/warthog618/modem/gsm"
 	"github.com/warthog618/modem/serial"
@@ -20,7 +30,7 @@ import (
 )
 
 func main() {
-	dev := flag.String("d", "/dev/ttyUSB0", "path to modem device")
+	dev := flag.String("d", "/dev/ttyUSB2", "path to modem device")
 	baud := flag.Int("b", 115200, "baud rate")
 	period := flag.Duration("p", 10*time.Minute, "period to wait")
 	timeout := flag.Duration("t", 400*time.Millisecond, "command timeout period")
@@ -40,6 +50,7 @@ func main() {
 		mio = trace.New(m, log.New(os.Stdout, "", log.LstdFlags))
 	}
 	g := gsm.New(mio)
+	g.SetPDUMode()
 	ctx, cancel := context.WithTimeout(context.Background(), *timeout)
 	err = g.Init(ctx)
 	cancel()
@@ -53,7 +64,6 @@ func main() {
 		return
 	}
 	ctx, cancel = context.WithTimeout(context.Background(), *timeout)
-	g.Command(ctx, "+CMGF=0") // switch to PDU MSG mode
 	if _, err = g.Command(ctx, "+CNMI=1,2,2,1,0"); err != nil {
 		log.Println(err)
 		cancel()
@@ -61,19 +71,63 @@ func main() {
 	}
 	cancel()
 	ctx, cancel = context.WithTimeout(context.Background(), *period)
+	go func() {
+		for {
+			select {
+			case <-time.After(time.Minute):
+				tctx, tcancel := context.WithTimeout(ctx, *timeout)
+				i, err := g.Command(tctx, "+CSQ")
+				if err != nil {
+					log.Println(err)
+				} else {
+					log.Printf("Signal quality: %v\n", i)
+				}
+				tcancel()
+			case <-ctx.Done():
+				return
+			}
+		}
+	}()
 	defer cancel()
+	pd := pdumode.Decoder{}
+	asyncError := func(error) {
+		log.Printf("reassembly error: %v", err)
+	}
+	udd, err := tpdu.NewUDDecoder()
+	if err != nil {
+		log.Printf("err: %v\n", err)
+	}
+	c := sar.NewCollector(time.Hour, asyncError)
+	reassembler := message.NewReassembler(udd, c)
+	defer reassembler.Close()
 	for {
 		select {
 		case <-ctx.Done():
 			log.Println("exiting...")
 			return
 		case i := <-cmt:
-			// at this point just dump the received SMS.
-			// TODO: do decoding and duplicate detection.
-			log.Println(i)
 			actx, acancel := context.WithTimeout(ctx, *timeout)
 			g.Command(actx, "+CNMA")
 			acancel()
+			lstr := strings.Split(i[0], ",")
+			l, err := strconv.Atoi(lstr[len(lstr)-1])
+			if err != nil {
+				log.Printf("err: %v\n", err)
+			}
+			_, pdu, err := pd.DecodeString(i[1])
+			if err != nil {
+				log.Printf("err: %v\n", err)
+			}
+			if int(l) != len(pdu) {
+				log.Printf("length mismatch - expected %d, got %d", l, len(pdu))
+			}
+			m, err := reassembler.Reassemble(pdu)
+			if err != nil {
+				log.Printf("err: %v\n", err)
+			}
+			if m != nil {
+				log.Printf("%s: %s\n", m.Number, m.Msg)
+			}
 		}
 	}
 }
